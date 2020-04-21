0aba06e7d14a09275d4454060ebe78bee154f0fd
Luke Fetterhoff <63879168+fetterhoffl@users.noreply.github.com>
2020-04-21 14:26:22 -0400

    Ball, physics, cup added
    ball added and code that when you click on the ball it pushes it forward if you hold click it goes further. use keys a and d to rotate the ball and added bounciness and a cup that takes you to the next scene. problem im having is the rotation of the ball on the x axis not freezing causing the ball to jump when hit.

---
+++ MiniGolf/Assets/Scripts/BallController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallController : MonoBehaviour
+{
+
+    public float zForce = 0;
+
+    public Camera MainCamera;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("a"))
+        {
+            transform.Rotate(0, -1, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, 1, 0);
+        }
+
+        MainCamera.GetComponent<Rigidbody>().velocity = GetComponent<Rigidbody>().velocity;
+    }
+
+    private void OnMouseDrag()
+    {
+        zForce += 10;
+        Debug.Log(zForce);
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<Rigidbody>().AddRelativeForce(0, 0, zForce);
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.name == "cup1")
+        {
+            SceneManager.LoadScene("hole2");
+        }
+    }
+}
